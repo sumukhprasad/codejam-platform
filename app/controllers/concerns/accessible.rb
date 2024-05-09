4de0840f962f83d3f0c382949fcee8e9d70612bb
Sumukh Prasad <sumukhprasad@icloud.com>
2024-05-10 04:31:00 +0530

    Linted code.

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -1,21 +1,21 @@
 module Accessible
-  extend ActiveSupport::Concern
-  included do
-    before_action :check_user
-  end
+	extend ActiveSupport::Concern
+	included do
+		before_action :check_user
+	end
 
-  protected
-  def check_user
-    if current_admin_user
-      flash.clear
-      redirect_to(admin_root_path) and return
-    elsif current_student
-      flash.clear
-      redirect_to(root_path) and return
-	 
+	protected
+	def check_user
+		if current_admin_user
+			flash.clear
+			redirect_to(admin_root_path) and return
+		elsif current_student
+			flash.clear
+			redirect_to(root_path) and return
+	
 	elsif current_institution
-   flash.clear
-   redirect_to(root_path) and return
-    end
-  end
+	flash.clear
+	redirect_to(root_path) and return
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,8 @@ Rails.application.routes.draw do
 		shared: "institutions/shared",
 		unlocks: "institutions/unlocks"
 	}
-  
-  
+	
+	
 	devise_for :admin_users, ActiveAdmin::Devise.config
 	ActiveAdmin.routes(self)
 	# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
