9c634a363f62d7dad836820834c79d334a9a7a9d
Sumukh Prasad <sumukhprasad@icloud.com>
2024-07-26 05:14:52 +0530

    Fixed bug in leaderboard.

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -2,6 +2,6 @@ include Pagy::Backend
 
 class LeaderboardController < ApplicationController
 	def index
-		@pagy, @scores = if !@scores.blank? then pagy_array(Rails.cache.fetch('leaderboard')) else nil end
+		@pagy, @scores = if !Rails.cache.fetch('leaderboard').blank? then pagy_array(Rails.cache.fetch('leaderboard')) else nil end
 	end
 end
