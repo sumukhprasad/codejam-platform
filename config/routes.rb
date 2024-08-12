1c64f5377393a3673c8d46f37f6df4b90b193f92
Sumukh Prasad <sumukhprasad@icloud.com>
2024-08-12 18:55:31 +0530

    Moved resque to admin.

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,9 @@ Rails.application.routes.draw do
 	devise_for :admin_users, ActiveAdmin::Devise.config
 	ActiveAdmin.routes(self)
 	
-	mount Resque::Server, :at => '/resque'
+	authenticate :admin_user do
+		mount Resque::Server, at: '/resque'
+	end
 	# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 	# Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
