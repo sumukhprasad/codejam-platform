098811ebf530f241b7dadb2cffff3b86741d244e
Sumukh Prasad <sumukhprasad@icloud.com>
2024-08-15 08:32:41 +0530

    Fixed leaderboard.

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -3,5 +3,7 @@ include Pagy::Backend
 class LeaderboardController < ApplicationController
 	def index
 		@pagy, @scores = if !Rails.cache.fetch('leaderboard').blank? then pagy_array(Rails.cache.fetch('leaderboard')) else nil end
+
+		@config = ContestConfiguration.where(active: true).first
 	end
 end
